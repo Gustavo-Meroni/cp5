8a58ca7a6b3ae4f626a6d4b16dd22755d00a877d
sbaecker2005 <samuelbaecker5@gmail.com>
2025-04-15 00:20:03 -0300

    fix: ajustando base vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/melodia-page/', 
+  base: '/cp5/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
