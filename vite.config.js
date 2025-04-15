e265a3453e6488f4f944858834bdfeb1ddb147dd
sbaecker2005 <samuelbaecker5@gmail.com>
2025-04-15 00:17:45 -0300

    fix: ajustando arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  base: '/melodia-page/', 
+  plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,  // ou outra porta disponível
+    port: 3000,
   },
 });
